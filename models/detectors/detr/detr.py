6d188d3a5b5f0c58e96041932bf347c5ce95f4c8
yjh0410 <1394571815@qq.com>
2022-08-21 18:17:16 +0800

    detr

--- models/detectors/detr/detr.py
+++ models/detectors/detr/detr.py
@@ -90,10 +90,11 @@ class DeTR(nn.Module):
             x_embed = x_embed / (x_embed[:, :, -1:] + eps) * scale
 
         dim_t = torch.arange(num_pos_feats, dtype=torch.float32, device=self.device)
-        dim_t = temperature ** (2 * (dim_t // 2) / num_pos_feats)
+        dim_t_ = torch.div(dim_t, 2, rounding_mode='floor') / num_pos_feats
+        dim_t = temperature ** (2 * dim_t_)
 
-        pos_x = x_embed[:, :, :, None] / dim_t
-        pos_y = y_embed[:, :, :, None] / dim_t
+        pos_x = torch.div(x_embed[:, :, :, None], dim_t)
+        pos_y = torch.div(y_embed[:, :, :, None], dim_t)
         pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
         pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
         # [B, d, H, W]
@@ -234,7 +235,7 @@ class DeTR(nn.Module):
             fmp_size = x.shape[-2:]
             if mask is not None:
                 # [B, H, W]
-                mask = F.interpolate(mask[None].float(), size=fmp_size).bool()
+                mask = F.interpolate(mask[None].float(), size=fmp_size).bool()[0]
             else:
                 mask = torch.zeros([x.shape[0], *x.shape[-2:]], device=x.device, dtype=torch.bool)
 
