422eba59687fd789d7416492f660dc5f466815a1
yjh0410 <1394571815@qq.com>
2022-08-21 18:16:06 +0800

    detr

--- models/detectors/detr/detr.py
+++ models/detectors/detr/detr.py
@@ -90,12 +90,10 @@ class DeTR(nn.Module):
             x_embed = x_embed / (x_embed[:, :, -1:] + eps) * scale
 
         dim_t = torch.arange(num_pos_feats, dtype=torch.float32, device=self.device)
-        dim_t_ = torch.div(dim_t, 2, rounding_mode='floor') / num_pos_feats
-        dim_t = temperature ** (2 * dim_t_)
-        print(x_embed.shape, dim_t.shape)
+        dim_t = temperature ** (2 * (dim_t // 2) / num_pos_feats)
 
-        pos_x = torch.div(x_embed[:, :, :, None], dim_t)
-        pos_y = torch.div(y_embed[:, :, :, None], dim_t)
+        pos_x = x_embed[:, :, :, None] / dim_t
+        pos_y = y_embed[:, :, :, None] / dim_t
         pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
         pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
         # [B, d, H, W]
