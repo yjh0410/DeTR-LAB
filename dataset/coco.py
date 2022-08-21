76f54d7312f88c5356e36f67bdcfc3fe0d33f39d
yjh0410 <1394571815@qq.com>
2022-08-21 15:11:03 +0800

    debug coco

--- dataset/coco.py
+++ dataset/coco.py
@@ -43,6 +43,7 @@ class CocoDetection(torchvision.datasets.CocoDetection):
         image_id = self.ids[idx]
         target = {'image_id': image_id, 'annotations': target}
         img, target = self.prepare(img, target)
+        print(self._transforms)
         if self._transforms is not None:
             img, target = self._transforms(img, target)
         return img, target
