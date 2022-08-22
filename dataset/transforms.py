cc0b8c571792c12a805c1b061c7354fe26a70c13
yjh0410 <1394571815@qq.com>
2022-08-22 14:07:20 +0800

    debug data

--- dataset/transforms.py
+++ dataset/transforms.py
@@ -266,7 +266,7 @@ class Compose(object):
     def __init__(self, transforms):
         self.transforms = transforms
 
-    def __call__(self, image, target=None):
+    def __call__(self, image, target):
         for t in self.transforms:
             image, target = t(image, target)
         return image, target
